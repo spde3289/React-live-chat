4d90277b9d802cc5833c15c725898e7b7cd56990
spde3289 <spde3289@naver.com>
2024-04-12 14:49:54 +0900

    feat: soket 연결

--- src/component/chatRoom/Room.tsx
+++ src/component/chatRoom/Room.tsx
@@ -1,11 +1,33 @@
+// import { useEffect } from "react";
+import io from "socket.io-client";
 import { useLocation } from "react-router-dom";
+import client from "../../fetch/backEnd";
 import CharRoom from "./chatRoom/chatRoom";
 import ParticipantCounter from "./ParticipantCounter/ParticipantCounter";
+
 export default function Room() {
   const location = useLocation();
 
+  const data = async () => {
+    try {
+      const s = await client.get("/");
+      const socket = io("http://localhost:3000", { transports: ["websocket"] });
+
+      socket.on("connect", () => {
+        console.log("connected to server");
+      });
+
+      socket.on("disconnect", () => {
+        console.log("disconnected from server");
+      });
+      console.log(s.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  data();
+
   const path: string = location.pathname.replace("/", "");
-  console.log(location);
   const decodedParameter = decodeURIComponent(path);
 
   return (
