218c77b931a5c27a3f63ff2f3d5598001933fedd
spde3289 <spde3289@naver.com>
2024-08-23 15:16:28 +0900

    .

--- src/component/Layout/sidebar/sidebarHeader.tsx
+++
@@ -1,9 +0,0 @@
-const SidebarHeader = () => {
-  return (
-    <div className="flex justify-center items-center w-[60px] h-[52px]">
-      <img src="/icon.png" />
-    </div>
-  );
-};
-
-export default SidebarHeader;
